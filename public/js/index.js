2836416909419cc006bba08944ebff164ac576b4
Simon-Sim <simon.sim@pathmazing.com>
2018-01-12 13:44:38 +0700

    update chat

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,50 @@ socket.on('disconnect', function () {
     console.log('disconnected');
 });
 
-socket.on('newEmail', function (email) {
-    console.log('New Email', email)
+socket.on('newMessage', function (message) {
+    console.log('New Message', message)
+    var formatTime = moment(message.createdAt).format('h:mm:a');
+    var li = $('<li></li>');
+    li.text(`${message.from} ${formatTime}: ${message.text}`);
+
+    $('#messages').append(li);
+});
+
+socket.on('newLocationMessage', function (message) {
+    var formatTime = moment(message.createdAt).format('h:mm:a');
+    var li = $('<li></li>');
+    var a =  $('<a target="_blank">My Location</a>');
+
+    li.text(`${message.from} ${formatTime}: `);
+    a.attr('href', message.url);
+    li.append(a);
+    $('#messages').append(li);
+});
+
+$('#chat-message').submit(function (e) {
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('#chat').val(),
+        createdAt: moment.valueOf()
+    }, function(message){
+        $('#chat').val('')
+    });
+});
+
+$('#send-location').on('click', function () {
+    if (!navigator.geolocation) {
+        return alert('not support');
+    }
+    $('#send-location').attr('disabled', 'disabled');
+    navigator.geolocation.getCurrentPosition(function (position) {
+        $('#send-location').removeAttr('disabled').text('Sending location...');
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function () {
+        $('#send-location').removeAttr('disabled').text('Sending location...');
+        alert('unable get location');
+    });
 });
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const {generateMessage, generateLocationMessage} = require('./utils/message.js');
 
 const publicPath = path.join(__dirname + './../public');
 const port = process.env.PORT || 3030;
@@ -14,13 +15,25 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('User connected');
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome'));
+
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user join'));
+
+    socket.on('createMessage', (message, callback)=>{
+        console.log('createMessage', message);
+
+        io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('this from server');
+        // socket.broadcast.emit('newMessage', generateMessage(message.from, message.text));
     });
 
-    socket.emit('newEmail', {
-        name: 'simon'
+    socket.on('createLocationMessage', (coords)=>{
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });    
 });
 server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
